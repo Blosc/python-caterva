8b3f909f9a82d239e689f7ce00c95d8b416ab57f
Francesc Alted <faltet@gmail.com>
2019-09-02 17:10:52 +0200

    Add new benchmarks on getitem

--- bench/compare_getitem.py
+++ bench/compare_getitem.py
@@ -1,7 +1,9 @@
 import cat4py as cat
+import zarr
 import numpy as np
 import os
 from time import time
+
 
 # Dimensions for big matrix
 shape = (500, 1000)
@@ -18,16 +20,22 @@ itemsize = np.dtype(dtype).itemsize
 
 # Create an empty caterva array (on disk)
 a = cat.empty(shape, pshape, itemsize=itemsize, filename=filename, compcode=0)
-
 # Fill an empty caterva array using a block iterator
 t0 = time()
-count = 0
+nchunks = 0
 for block, info in a.iter_write():
-    nparray = np.arange(count, count + info.size, dtype=dtype)
+    nparray = np.arange(nchunks * info.size, (nchunks + 1) * info.size, dtype=dtype)
     block[:] = bytes(nparray)
-    count += info.size
+    nchunks += 1
 t1 = time()
-print("Time for filling: %.3fs" % (t1 - t0))
+print("Time for filling caterva array: %.3fs" % (t1 - t0))
+
+t0 = time()
+z = zarr.empty(shape, chunks=pshape, dtype=dtype)
+for block, info in a.iter_read(pshape):
+    z[info.slice] = np.frombuffer(block, dtype=dtype).reshape(pshape)
+t1 = time()
+print("Time for filling zarr array: %.3fs" % (t1 - t0))
 
 # Check that the retrieved items are correct
 t0 = time()
@@ -39,13 +47,22 @@ for block, info in a.iter_read(pshape):
 t1 = time()
 print("Time for reading with iterator: %.3fs" % (t1 - t0))
 
-# Use getitem
+# Time getitem with caterva
 t0 = time()
 for i in range(shape[0]):
     rbytes = a[i]
-    # print(np.frombuffer(rbytes, dtype=dtype).reshape((1, shape[1])))
+    block = np.frombuffer(rbytes, dtype=dtype).reshape((1, shape[1]))
+    # print(block)
 t1 = time()
-print("Time for reading with getitem: %.3fs" % (t1 - t0))
+print("Time for reading with getitem (caterva): %.3fs" % (t1 - t0))
+
+# Time getitem with zarr
+t0 = time()
+for i in range(shape[0]):
+    block = z[i]
+    # print(block)
+t1 = time()
+print("Time for reading with getitem (zarr): %.3fs" % (t1 - t0))
 
 
 if filename is not None:
