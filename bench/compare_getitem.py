a57f14d84f708294e080baba0e4e454ddbadaf72
Francesc Alted <faltet@gmail.com>
2019-09-03 11:43:10 +0200

    Using a 3-dim array and random slice selection

--- bench/compare_getitem.py
+++ bench/compare_getitem.py
@@ -1,6 +1,6 @@
 import cat4py as cat
 import zarr
-from numcodecs import Blosc
+import numcodecs
 import tables
 import numpy as np
 import os
@@ -9,16 +9,16 @@ from time import time
 
 
 # Dimensions, type and persistency properties for the arrays
-shape = (500, 10000)
-pshape = (50, 1000)
+shape = (50, 5000, 100)
+pshape = (10, 50, 20)
 dtype = np.float64
 persistent = False
 
 # Compression properties
 cname = "lz4"
-compcode = 1  # keep in sync with above
+compcode = 1 # keep in sync with above
 clevel = 5
-filter = Blosc.SHUFFLE
+filter = numcodecs.Blosc.SHUFFLE
 
 fname_cat = None
 fname_zarr = None
@@ -35,7 +35,7 @@ if persistent:
         os.remove(fname_h5)
 
 # Create content for populating arrays
-content = np.linspace(0, 10, shape[0] * shape[1], dtype=dtype).reshape(shape)
+content = np.linspace(0, 10, int(np.prod(shape)), dtype=dtype).reshape(shape)
 
 itemsize = np.dtype(dtype).itemsize
 
@@ -61,11 +61,11 @@ print("Time for filling array (caterva, from_buffer): %.3fs" % (t1 - t0))
 
 # Create and fill a zarr array
 t0 = time()
-compressor = Blosc(cname=cname, clevel=clevel, shuffle=Blosc.SHUFFLE)
+compressor = numcodecs.Blosc(cname=cname, clevel=clevel, shuffle=numcodecs.Blosc.SHUFFLE)
 if persistent:
-    z = zarr.open(fname_zarr, mode='w', shape=shape, chunks=pshape, dtype=dtype)
+    z = zarr.open(fname_zarr, mode='w', shape=shape, chunks=pshape, dtype=dtype, compressor=compressor)
 else:
-    z = zarr.empty(shape=shape, chunks=pshape, dtype=dtype)
+    z = zarr.empty(shape=shape, chunks=pshape, dtype=dtype, compressor=compressor)
 z[:] = content
 t1 = time()
 print("Time for filling array (zarr): %.3fs" % (t1 - t0))
@@ -74,10 +74,10 @@ print("Time for filling array (zarr): %.3fs" % (t1 - t0))
 t0 = time()
 filters = tables.Filters(complevel=clevel, complib="blosc:%s" % cname, shuffle=True)
 if persistent:
-    h5f = tables.open_file(fname_h5, 'w', filters=filters)
+    h5f = tables.open_file(fname_h5, 'w')
 else:
-    h5f = tables.open_file(fname_h5, 'w', filters=filters, driver='H5FD_CORE')
-h5ca = h5f.create_carray(h5f.root, 'carray', chunkshape=pshape, obj=content)
+    h5f = tables.open_file(fname_h5, 'w', driver='H5FD_CORE')
+h5ca = h5f.create_carray(h5f.root, 'carray', filters=filters, chunkshape=pshape, obj=content)
 h5f.flush()
 t1 = time()
 print("Time for filling array (hdf5): %.3fs" % (t1 - t0))
@@ -93,18 +93,21 @@ for block, info in a.iter_read(pshape):
 t1 = time()
 print("Time for checking contents: %.3fs" % (t1 - t0))
 
+# Get the coordinates for random planes
+planes_idx = np.random.randint(0, shape[1], 100)
+
 # Time getitem with caterva
 t0 = time()
-for i in range(shape[0]):
-    rbytes = a[i]
-    block = np.frombuffer(rbytes, dtype=dtype).reshape((1, shape[1]))
+for i in planes_idx:
+    rbytes = a[:,i,:]
+    block = np.frombuffer(rbytes, dtype=dtype).reshape((shape[0], shape[2]))
 t1 = time()
 print("Time for reading with getitem (caterva): %.3fs" % (t1 - t0))
 
 # Time getitem with zarr
 t0 = time()
-for i in range(shape[0]):
-    block = z[(i,)]
+for i in planes_idx:
+    block = z[:,i,:]
 t1 = time()
 print("Time for reading with getitem (zarr): %.3fs" % (t1 - t0))
 
@@ -114,8 +117,8 @@ if persistent:
     h5f.close()
     h5f = tables.open_file(fname_h5, 'r', filters=filters)
 h5ca = h5f.root.carray
-for i in range(shape[0]):
-    block = h5ca[i]
+for i in planes_idx:
+    block = h5ca[:,i,:]
 h5f.close()
 t1 = time()
 print("Time for reading with getitem (hdf5): %.3fs" % (t1 - t0))
