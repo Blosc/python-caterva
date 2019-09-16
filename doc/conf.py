f6b81fd7eead1595dbe7023238479df5b4db22ec
aleix11alcacer <al312972@uji.es>
2019-09-16 12:17:57 +0200

    Refeactorisation

--- doc/conf.py
+++ doc/conf.py
@@ -57,16 +57,12 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
 html_logo = "logo-caterva.png"
 html_favicon = "favicon-caterva.png"
-
 html_theme_options = {
     "logo_only": True,
 }
-
 html_show_sourcelink = False
-
 autodoc_member_order = 'groupwise'
 
 
