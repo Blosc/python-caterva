48f2aaa6b709b1fc83fccdabab3f52026922c7cc
Francesc Alted <faltet@gmail.com>
2019-09-20 10:21:11 +0200

    Workaround for a slight increase of sframes

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -4,14 +4,15 @@ import numpy as np
 import os
 
 
-@pytest.mark.parametrize("shape, pshape, itemsize, memframe, filename",
+@pytest.mark.parametrize("shape, pshape, itemsize, memframe, filename, copy",
                          [
-                             ([200], [20], 8, True, None),
-                             ([20, 134, 13], [3, 13, 5], 4, False, None),
-                             ([20, 134, 13], [3, 13, 5], 4, False, 'test_frame.cat'),
-                             ([12, 13, 14, 15, 16], [2, 3, 4, 4, 4], 8, True, None)
+                             ([200], [20], 8, True, None, True),
+                             ([200], [20], 8, True, None, False),
+                             ([20, 134, 13], [3, 13, 5], 4, False, None, True),
+                             ([20, 134, 13], [3, 13, 5], 4, False, 'test_frame.cat', False),
+                             ([12, 13, 14, 15, 16], [2, 3, 4, 4, 4], 8, True, None, True)
                          ])
-def test_frame(shape, pshape, itemsize, memframe, filename):
+def test_frame(shape, pshape, itemsize, memframe, filename, copy):
     if filename is not None and os.path.exists(filename):
         os.remove(filename)
 
@@ -19,19 +20,27 @@ def test_frame(shape, pshape, itemsize, memframe, filename):
     buffer = bytes(size * itemsize)
     a = cat.from_buffer(buffer, shape, pshape=pshape, itemsize=itemsize,
                         memframe=memframe, filename=filename)
-    frame1 = a.to_sframe()
+    sframe1 = a.to_sframe()
     buffer1 = a.to_buffer()
-    # Size of a compressed frame should be less than the plain buffer for
-    # the cases here
-    # print("->", len(frame1), len(buffer1))
-    assert len(frame1) < len(buffer1)
+    # Size of a compressed frame should be less than the plain buffer for the cases here
+    assert len(sframe1) < len(buffer1)
 
-    b = cat.from_sframe(frame1, copy=True)
-    frame2 = b.to_sframe()
-    # TODO: the next assert currently fails.  Investigate...
-    # assert len(frame2) == len(frame1)
+    b = cat.from_sframe(sframe1, copy=copy)
+    sframe2 = b.to_sframe()
+    # For some reason, the size of sframe1 and sframe2 are not equal when copies are made,
+    # but the important thing is that the length of the frame should be stable in multiple
+    # round-trips after the first one.
+    # assert len(sframe2) == len(sframe1)
+    sframe3 = sframe2
+    c = b
+    for i in range(10):
+        c = cat.from_sframe(sframe3, copy=copy)
+        sframe3 = c.to_sframe()
+    assert len(sframe3) == len(sframe2)
     buffer2 = b.to_buffer()
     assert buffer2 == buffer1
+    buffer3 = c.to_buffer()
+    assert buffer3 == buffer1
 
     if filename is not None and os.path.exists(filename):
         os.remove(filename)
