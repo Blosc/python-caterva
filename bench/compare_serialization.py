506975bbd7045e52d01b7e87ff23ede47c596390
Francesc Alted <faltet@gmail.com>
2019-09-23 12:28:35 +0200

    Add a comparison against arrow serialization

--- bench/compare_serialization.py
+++ bench/compare_serialization.py
@@ -2,14 +2,18 @@
 # multidimensional arrays using different methods:
 # * to_sframe() / from_sframe()
 # * Numpy copy
+# * PyArrow (shallow copy)
 # * Pickle v5 (in the future)
 
 import cat4py as cat
 import numpy as np
 from time import time
+import pyarrow as pa
 
 # import pickle
 # assert(pickle.HIGHEST_PROTOCOL < 5)
+
+check_roundtrip = True  # set this to True to check for roundtrip validity
 
 # Dimensions, type and persistency properties for the arrays
 shape = (100, 5000, 250)
@@ -55,23 +59,54 @@ print()
 t0 = time()
 sframe = carr.to_sframe()
 t1 = time()
-print("Time for serializing array in-memory (caterva): %.3fs" % (t1 - t0))
+print("Time for serializing array in-memory (caterva, copy): %.3fs" % (t1 - t0))
+
+t0 = time()
+serialized = pa.serialize(arr)
+components = serialized.to_components()
+t1 = time()
+print("Time for serializing array in-memory (arrow, no-copy): %.3fs" % (t1 - t0))
 
 t0 = time()
 carr2 = cat.from_sframe(sframe)
 t1 = time()
-print("Time for de-serializing array in-memory (caterva): %.3fs" % (t1 - t0))
+print("Time for de-serializing array in-memory (caterva, no-copy): %.3fs" % (t1 - t0))
+
+# Activate this when we would have a proper NPArray class with an __array__ method
+# if check_roundtrip:
+#     print("Checking that the roundtrip is... ", end="")
+#     np.testing.assert_allclose(carr2, arr)
+#     print("ok!")
+
+t0 = time()
+arr2 = pa.deserialize_components(components)
+t1 = time()
+print("Time for de-serializing array in-memory (arrow, no-copy): %.3fs" % (t1 - t0))
+
+if check_roundtrip:
+    print("Checking that the roundtrip is... ", end="")
+    np.testing.assert_allclose(arr2, arr)
+    print("ok!")
+
+t0 = time()
+arr2 = pa.deserialize_components(components).copy()
+t1 = time()
+print("Time for de-serializing array in-memory (arrow, copy): %.3fs" % (t1 - t0))
+
+if check_roundtrip:
+    print("Checking that the roundtrip is... ", end="")
+    np.testing.assert_allclose(arr2, arr)
+    print("ok!")
 
 t0 = time()
 for i in range(1):
     carr3 = cat.from_sframe(sframe)
     dtype_deserialized = carr3.get_metalayer("numpy")
-    arr3 = carr3.to_numpy(dtype=dtype_deserialized)
+    arr2 = carr3.to_numpy(dtype=dtype_deserialized)
 t1 = time()
-print("Time for re-creating array in-memory (caterva -> numpy): %.3fs" % (t1 - t0))
+print("Time for re-creating array in-memory (caterva -> numpy, copy): %.3fs" % (t1 - t0))
 
-# print("Checking that the deserialization is ...")
-# t0 = time()
-# np.testing.assert_allclose(arr2, arr3)
-# t1 = time()
-# print("ok!")
+if check_roundtrip:
+    print("Checking that the roundtrip is... ", end="")
+    np.testing.assert_allclose(arr2, arr)
+    print("ok!")
