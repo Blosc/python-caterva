8d31948894874eba29201d7d1dacd6865744e16a
Francesc Alted <faltet@gmail.com>
2019-09-27 15:18:06 +0200

    Make all the examples to work

--- examples/ex_npcontainer.py
+++ examples/ex_npcontainer.py
@@ -1,6 +1,5 @@
 import cat4py as cat
 import numpy as np
-import os
 from itertools import zip_longest as lzip
 
 pshape = (5, 5)
@@ -28,9 +27,6 @@ for block, info in b.iter_write():
 for (block1, info1), (block2, info2) in lzip(a.iter_read(blockshape), b.iter_read(blockshape)):
     np.testing.assert_almost_equal(block1, block2)
 
-b[2:6, 4:6] = np.ones((4, 2), dtype=dtype)
-
 print(b.to_numpy())
-
 print(b[5:10, 5:10])
 
