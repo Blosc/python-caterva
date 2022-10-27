fe274f61c1a52317c08d65e46cfd2fa61f01ce35
oscargm98 <soscargm98@gmail.com>
2022-10-27 13:30:32 +0200

    update submodules

--- caterva/caterva_ext.pyx
+++ caterva/caterva_ext.pyx
@@ -47,7 +47,7 @@ cdef extern from "blosc2.h":
         BLOSC2_MAX_FILTERS
         BLOSC2_MAX_METALAYERS
         BLOSC2_MAX_VLMETALAYERS
-        BLOSC_MAX_OVERHEAD
+        BLOSC2_MAX_OVERHEAD
         BLOSC_ALWAYS_SPLIT = 1,
         BLOSC_NEVER_SPLIT = 2,
         BLOSC_AUTO_SPLIT = 3,
@@ -194,7 +194,8 @@ cdef extern from "caterva.h":
                                  int64_t *start, int64_t *stop, caterva_array_t *array);
     int caterva_copy(caterva_ctx_t *ctx, caterva_array_t *src, caterva_storage_t *storage,
                      caterva_array_t ** array);
-    int caterva_resize(caterva_ctx_t *ctx, caterva_array_t *array, int64_t *new_shape);
+    int caterva_resize(caterva_ctx_t *ctx, caterva_array_t *array, int64_t *new_shape,
+                       int64_t *start);
 
 # Defaults for compression params
 config_dflts = {
@@ -319,7 +320,7 @@ cdef class NDArray:
     @property
     def cratio(self):
         """The compression ratio for this container."""
-        return self.size / (self.array.sc.cbytes + BLOSC_MAX_OVERHEAD * self.nchunks)
+        return self.size / (self.array.sc.cbytes + BLOSC2_MAX_OVERHEAD * self.nchunks)
 
     @property
     def clevel(self):
@@ -519,7 +520,7 @@ def resize(NDArray arr, new_shape):
     cdef int64_t new_shape_[CATERVA_MAX_DIM]
     for i, s in enumerate(new_shape):
         new_shape_[i] = s
-    caterva_resize(ctx.context_, arr.array, new_shape_)
+    caterva_resize(ctx.context_, arr.array, new_shape_, NULL)
     return arr
 
 def from_file(NDArray arr, urlpath, **kwargs):
